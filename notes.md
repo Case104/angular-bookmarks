f22f8cc8ddf92b2ec32548cea0e0a37f655f7761
Case104 <jpcase104@gmail.com>
2016-10-14 13:55:45 -0400

    Refactor from single controller

---
+++ notes.md
@@ -0,0 +1,7 @@
+To Dos:
+
+Source all dependencies from vendor rather than cdn (or integrate webpack).
+
+Refactor away from eggly and to my own todo app.
+
+Persist data to a database or create a rails backend.
---
+++ src/app/categories/bookmarks/create/bookmark-create.js
@@ -0,0 +1,44 @@
+// CHECKED
+angular.module('categories.bookmarks.create', [
+
+])
+	.config(function($stateProvider){
+		$stateProvider
+			.state('eggly.categories.bookmarks.create', {
+				url: '/bookmarks/create',
+				templateUrl: 'app/categories/bookmarks/create/bookmark-create.tmpl.html',
+				controller: 'CreateBookmarkCtrl as createBookmarkCtrl'
+			})
+		;
+	})
+	.controller('CreateBookmarkCtrl', function($state, $stateParams, BookmarksModel){
+		var createBookmarkCtrl = this;
+
+		function returnToBookmarks(){
+			$state.go('eggly.categories.bookmarks', {
+				category: $stateParams.category
+			})
+		}
+
+		function cancelCreating(){
+			returnToBookmarks();
+		}
+
+		function createBookmark(){
+			Bookmarksmodel.createBookmark(createBookmarkCtrl(bookmark));
+			returnToBookmarks();
+		}
+
+		function resetForm(){
+			createBookmarkCtrl.newBookmark = {
+				title: '',
+				url: '',
+				category: $stateParams.category
+			};
+		}
+
+		createBookmarkCtrl.cancelCreating = cancelCreating;
+		createBookmarkCtrl.createBookmark = createBookmark;
+
+		resetForm();
+	});
